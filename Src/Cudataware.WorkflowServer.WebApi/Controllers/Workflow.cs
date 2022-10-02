d2fcc65f2e8e6b28bae83757204e23dfc82ecd1f
Denny Rodriguez Fajardo <dennitorf@gmail.com>
2022-10-02 01:37:27 -0400

    adding controller to test workflow execution

---
+++ Src/Cudataware.WorkflowServer.WebApi/Controllers/Workflow.cs
@@ -0,0 +1,21 @@
+﻿using Cudataware.WorkflowServer.Application.Features.Workflows.Commands.WorkflowExecution;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Cudataware.WorkflowServer.WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class Workflow : BaseController
+    {        
+        [HttpPost]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        public async Task<IActionResult> Post(WorkflowExecutionCommand command)
+        {
+            return Ok(await Mediator.Send(command));
+        }
+        
+    }
+}
